91668bdfa9199f5d6fee8798b0835229d19df7db
Meghana <meghana.sri.satya@gmail.com>
2023-07-26 18:48:19 -0500

    Commit after completing Task1 and bonus

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint,getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        dataPoint = (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2)
+        self.assertEqual(getDataPoint(quote), dataPoint)
+        self.assertEqual(1, 1)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +19,31 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+       dataPoint = (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'],(quote['top_bid']['price'] + quote['top_ask']['price']) / 2)
+       self.assertEqual(getDataPoint(quote), dataPoint)
+       self.assertEqual(1, 1)
 
   """ ------------ Add more unit tests ------------ """
 
 
+  def test_getRatio_normalCase(self):
+     price_a = 10.0
+     price_b = 5.0
+     expected_ratio = price_a / price_b
+     self.assertEqual(getRatio(price_a, price_b), expected_ratio)
+
+  def test_getRatio_priceBisZero(self):
+     price_a = 10.0
+     price_b = 0.0
+     # When price_b is zero, the ratio should return None to avoid division by zero.
+     self.assertIsNone(getRatio(price_a, price_b))
+
+  def test_getRatio_priceAisZero(self):
+   price_a = 0.0
+   price_b = 5.0
+  # When price_a is zero, the ratio should be 0.
+   self.assertEqual(getRatio(price_a, price_b), 0)
 
 if __name__ == '__main__':
     unittest.main()
